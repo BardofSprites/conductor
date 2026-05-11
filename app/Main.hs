74300acea6a80fa6d542ae5b13d83605cb6a3f16
BardofSprites <89086143+BardofSprites@users.noreply.github.com>
2026-05-11 10:49:48 -0400

    better example for main testing

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,6 @@ module Main where
 
 import System.Environment (getArgs)
 import Data.Maybe (catMaybes)
-import Song (readSong)
 import Song
 import Syntax
 import Commands.Limit
@@ -12,9 +11,29 @@ import Files
 import qualified Evaluators.LimitEvaluator as L
 import qualified Evaluators.SortEvaluator as S
 
--- Entry point
+printFormatted :: Song -> IO ()
+printFormatted song = putStrLn ("[" ++
+                                (show . year) song ++
+                                "] " ++
+                                artist song ++
+                                " - " ++
+                                title song ++
+                                " (" ++
+                                (show . duration) song ++
+                                ")")
+
 main :: IO ()
 main = do
   args <- getArgs
-  songs <- catMaybes <$> mapM readSong args
-  mapM_ print songs
+  case args of
+    [dir] -> do
+      filePaths <- getAllFiles dir ["git"]
+      maybeSongs <- mapM readSong filePaths
+      let songs = catMaybes maybeSongs
+          limitExpression = LimitExpr Nothing (Just 2000) Nothing
+          limitedSongs = L.applyLimit limitExpression songs
+          sortExpression = SortExpr Year Asc
+          sortedSongs = S.applySort sortExpression limitedSongs
+      mapM_ printFormatted sortedSongs
+
+    _ -> putStrLn "Usage: conductor <music-directory-path>"
